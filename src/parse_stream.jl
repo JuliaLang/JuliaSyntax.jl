76e43de091d561a5beee00d7617baf4306cf89a2
KristofferC <kristoffer.carlsson@juliacomputing.com>
2022-02-23 12:27:00 +0100

    get rid of the `TokenError` field in `Token`

--- src/parse_stream.jl
+++ src/parse_stream.jl
@@ -55,7 +55,7 @@ function Base.summary(head::SyntaxHead)
 end
 
 function untokenize(head::SyntaxHead; unique=true, include_flag_suff=true)
-    str = untokenize(kind(head); unique=unique)
+    str = is_error(kind(head)) ? "error" : untokenize(kind(head); unique=unique)
     if is_dotted(head)
         str = "."*str
     end
