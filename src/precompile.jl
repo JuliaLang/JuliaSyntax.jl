9ad37039816fddad061c40233893299e96f57e8a
Kristoffer Carlsson <kristoffer.carlsson@juliacomputing.com>
2023-03-10 07:14:31 +0100

    Update src/precompile.jl
    Co-authored-by: c42f <chris42f@gmail.com>

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,6 +1,5 @@
 # Just parse some file as a precompile workload
 let filename = joinpath(@__DIR__, "literal_parsing.jl")
     text = read(filename, String)
-    stream = JuliaSyntax.ParseStream(text)
-    JuliaSyntax.parse!(stream)
+    parseall(Expr, text)
 end
