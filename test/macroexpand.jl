42ce78776bd6c76d05298ffd26ad3ee2db32bac4
Claire Foster <aka.c42f@gmail.com>
2023-07-07 20:19:13 +1000

    Handle macros which call macros more correctly
    Macros expansion of macros which emit macros as part of their AST needs
    care when that AST contains interpolations: We don't want the second
    macro expansion to see the hygenic_scope expression. One way to do this
    is to transform hygenic_scope back into `SyntaxLiteral` on the fly.
    
    Also fix auto-escaping for expressions arising from the original scope
    by adding the necessary number of Expr(:esc)

--- test/macroexpand.jl
+++ test/macroexpand.jl
@@ -4,11 +4,13 @@ JuliaSyntax.include2(Main, "macroexpand_inc.jl")
     loc = A.LocationMacros.loc()
     @test loc.mod == A.LocationMacros
     @test last(splitpath(loc.file)) ==  "macroexpand_inc.jl"
-    @test loc.line == 24
+    @test loc.line == 26
     @test loc.column == 72
 
     @test A.hygiene_test() == ("x in B", "z in A")
 
     @test A.old_macro_test(1) == 3
     @test A.old_macro_test(2) == 5
+
+    @test A.macro_calling_macro_test(3) == 27
 end
