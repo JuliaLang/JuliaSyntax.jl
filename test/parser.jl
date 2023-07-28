38a4009e49aeb67f8cf82265e4fe9f1a52453a29
Lilith Hafner <Lilith.Hafner@gmail.com>
2023-07-28 17:03:41 -0400

    more tests

--- test/parser.jl
+++ test/parser.jl
@@ -952,8 +952,10 @@ tests = [
         "public export=true foo, bar"                   => PARSE_ERROR # but these may be
         "public experimental=true foo, bar"             => PARSE_ERROR # supported soon ;)
         "public(x::String) = false"                     => "(= (call public (::-i x String)) false)"
-        "module M; export @a; macro a end; end"         => "(module M (block (export @a) (macro a)))"
-        "module M; public @a; macro a end; end"         => "(module M (block (public @a) (macro a)))"
+        "module M; export @a; end"                      => "(module M (block (export @a)))"
+        "module M; public @a; end"                      => "(module M (block (public @a)))"
+        "module M; export ⤈; end"                       => "(module M (block (export ⤈)))"
+        "module M; public ⤈; end"                       => "(module M (block (public ⤈)))"
     ],
     JuliaSyntax.parse_docstring => [
         """ "notdoc" ]        """ => "(string \"notdoc\")"
