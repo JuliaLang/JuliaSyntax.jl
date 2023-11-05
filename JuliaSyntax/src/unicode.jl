a5233d8ddece65769d628866af3398b0a4e1b946
Steven G. Johnson <stevenj@alum.mit.edu>
2023-11-04 21:03:52 -0400

    port Base.is_id_char and Base.is_id_start_char

--- JuliaSyntax/src/unicode.jl
+++ JuliaSyntax/src/unicode.jl
@@ -37,8 +37,11 @@ function category_code(c::AbstractChar)
     !ismalformed(c) ? category_code(UInt32(c)) : Cint(31)
 end
 
+# doesn't check validity of x:
+@inline _category_code(x::Integer) = ccall((:utf8proc_category,libutf8proc), Cint, (UInt32,), x)
+
 function category_code(x::Integer)
-    x ≤ 0x10ffff ? ccall((:utf8proc_category,libutf8proc), Cint, (UInt32,), x) : Cint(30)
+    x ≤ 0x10ffff ? _category_code(x) : Cint(30)
 end
 
 @inline isspace(c::AbstractChar) =
@@ -90,16 +93,99 @@ end
 #####################################################################
 # Julia identifier parsing predicates
 
-function is_identifier_char(c::Char)
-    # c == EOF_CHAR && return false # covered by isvalid check
-    isvalid(c) || return false
-    return Base.is_id_char(c)
+# port of is_wc_cat_id_start from julia/src/flisp/julia_extensions.c
+function _is_identifier_start_char(c::UInt32, cat::Integer)
+    return (cat == UTF8PROC_CATEGORY_LU || cat == UTF8PROC_CATEGORY_LL ||
+            cat == UTF8PROC_CATEGORY_LT || cat == UTF8PROC_CATEGORY_LM ||
+            cat == UTF8PROC_CATEGORY_LO || cat == UTF8PROC_CATEGORY_NL ||
+            cat == UTF8PROC_CATEGORY_SC ||  # allow currency symbols
+            # other symbols, but not arrows or replacement characters
+            (cat == UTF8PROC_CATEGORY_SO && !(c >= 0x2190 && c <= 0x21FF) &&
+             c != 0xfffc && c != 0xfffd &&
+             c != 0x233f &&  # notslash
+             c != 0x00a6) || # broken bar
+
+            # math symbol (category Sm) whitelist
+            (c >= 0x2140 && c <= 0x2a1c &&
+             ((c >= 0x2140 && c <= 0x2144) || # ⅀, ⅁, ⅂, ⅃, ⅄
+              c == 0x223f || c == 0x22be || c == 0x22bf || # ∿, ⊾, ⊿
+              c == 0x22a4 || c == 0x22a5 ||   # ⊤ ⊥
+
+              (c >= 0x2200 && c <= 0x2233 &&
+               (c == 0x2202 || c == 0x2205 || c == 0x2206 || # ∂, ∅, ∆
+                c == 0x2207 || c == 0x220e || c == 0x220f || # ∇, ∎, ∏
+                c == 0x2200 || c == 0x2203 || c == 0x2204 || # ∀, ∃, ∄
+                c == 0x2210 || c == 0x2211 || # ∐, ∑
+                c == 0x221e || c == 0x221f || # ∞, ∟
+                c >= 0x222b)) || # ∫, ∬, ∭, ∮, ∯, ∰, ∱, ∲, ∳
+
+              (c >= 0x22c0 && c <= 0x22c3) ||  # N-ary big ops: ⋀, ⋁, ⋂, ⋃
+              (c >= 0x25F8 && c <= 0x25ff) ||  # ◸, ◹, ◺, ◻, ◼, ◽, ◾, ◿
+
+              (c >= 0x266f &&
+               (c == 0x266f || c == 0x27d8 || c == 0x27d9 || # ♯, ⟘, ⟙
+                (c >= 0x27c0 && c <= 0x27c1) ||  # ⟀, ⟁
+                (c >= 0x29b0 && c <= 0x29b4) ||  # ⦰, ⦱, ⦲, ⦳, ⦴
+                (c >= 0x2a00 && c <= 0x2a06) ||  # ⨀, ⨁, ⨂, ⨃, ⨄, ⨅, ⨆
+                (c >= 0x2a09 && c <= 0x2a16) ||  # ⨉, ⨊, ⨋, ⨌, ⨍, ⨎, ⨏, ⨐, ⨑, ⨒, ⨓, ⨔, ⨕, ⨖
+                c == 0x2a1b || c == 0x2a1c)))) || # ⨛, ⨜
+
+            (c >= 0x1d6c1 && # variants of \nabla and \partial
+             (c == 0x1d6c1 || c == 0x1d6db ||
+              c == 0x1d6fb || c == 0x1d715 ||
+              c == 0x1d735 || c == 0x1d74f ||
+              c == 0x1d76f || c == 0x1d789 ||
+              c == 0x1d7a9 || c == 0x1d7c3)) ||
+
+            # super- and subscript +-=()
+            (c >= 0x207a && c <= 0x207e) ||
+            (c >= 0x208a && c <= 0x208e) ||
+
+            # angle symbols
+            (c >= 0x2220 && c <= 0x2222) || # ∠, ∡, ∢
+            (c >= 0x299b && c <= 0x29af) || # ⦛, ⦜, ⦝, ⦞, ⦟, ⦠, ⦡, ⦢, ⦣, ⦤, ⦥, ⦦, ⦧, ⦨, ⦩, ⦪, ⦫, ⦬, ⦭, ⦮, ⦯
+
+            # Other_ID_Start
+            c == 0x2118 || c == 0x212E || # ℘, ℮
+            (c >= 0x309B && c <= 0x309C) || # katakana-hiragana sound marks
+
+            # bold-digits and double-struck digits
+            (c >= 0x1D7CE && c <= 0x1D7E1)) # 𝟎 through 𝟗 (inclusive), 𝟘 through 𝟡 (inclusive)
 end
 
+# from jl_id_start_char in julia/src/flisp/julia_extensions.c
 function is_identifier_start_char(c::Char)
-    # c == EOF_CHAR && return false # covered by isvalid check
-    isvalid(c) || return false
-    return Base.is_id_start_char(c)
+    if (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_'
+        return true
+    end
+    if c < Char(0xA1) || !isvalid(c)
+        return false
+    end
+    x = UInt32(c)
+    return _is_identifier_start_char(x, _category_code(x))
+end
+
+# from jl_id_char in julia/src/flisp/julia_extensions.c
+function is_identifier_char(c::Char)
+    if (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_' ||
+       (c >= '0' && c <= '9') || c == '!'
+        return true
+    end
+    if c < Char(0xA1) || !isvalid(c)
+        return false
+    end
+    x = UInt32(c)
+    cat = _category_code(x)
+    _is_identifier_start_char(x, cat) && return true
+    if (cat == UTF8PROC_CATEGORY_MN || cat == UTF8PROC_CATEGORY_MC ||
+        cat == UTF8PROC_CATEGORY_ND || cat == UTF8PROC_CATEGORY_PC ||
+        cat == UTF8PROC_CATEGORY_SK || cat == UTF8PROC_CATEGORY_ME ||
+        cat == UTF8PROC_CATEGORY_NO ||
+        # primes (single, double, triple, their reverses, and quadruple)
+        (x >= 0x2032 && x <= 0x2037) || (x == 0x2057))
+        return true
+    end
+    return false
 end
 
 #####################################################################
